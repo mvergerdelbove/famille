de1025ab674d337207d2765f6d6a456423fb333c
mvergerdelbove <mvergerdelbove@work4labs.com>
2013-12-21 10:50:41 +0100

    manage.py always executed with venv/bin/python

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python
+#!/bin/bash
+
+# coding=utf-8
+
+"true" '''\'
+exec "$(dirname $0)/venv/bin/python" "$0" "$@"
+'''
+
 import os
 import sys
 
-if __name__ == "__main__":
+
+if  __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "famille.settings")
 
     from django.core.management import execute_from_command_line
