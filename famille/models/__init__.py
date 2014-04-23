46c9a2938968c0e995c5bac5c2377383d96eae03
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-04-23 22:14:08 +0200

    Revert "backend models for messages"
    This reverts commit 261ef2691431aa7b260c657d6a533c76fcf7797d.
    
    Conflicts:
    	famille/models/messages.py
    	famille/tests/test_models.py

--- famille/models/__init__.py
+++ famille/models/__init__.py
@@ -5,7 +5,6 @@ from famille.utils import fields as extra_fields
 from .users import *
 from .planning import *
 from .rating import *
-from .messages import *
 
 # south rules
 add_introspection_rules(extra_fields.content_type_restricted_file_field_rules, ["^famille\.utils\.fields", ])
