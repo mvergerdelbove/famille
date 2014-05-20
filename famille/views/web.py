a5d5a763c58f841c23a07a163512a9aed1f8637c
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-05-21 00:59:23 +0200

    user cannot vote for themselves

--- famille/views/web.py
+++ famille/views/web.py
@@ -185,7 +185,8 @@ def profile(request, type, uid):
 
     if has_user_related(request.user):
         related_user = get_user_related(request.user)
-        if not RatingClass.user_has_voted_for(related_user, user):
+        # users cant vote for themselves or twice for a user
+        if user != related_user and not RatingClass.user_has_voted_for(related_user, user):
             rating = RatingClass(user=user, by=related_user.simple_id)
             context["rating_form"] = RatingFormClass(instance=rating)
 
