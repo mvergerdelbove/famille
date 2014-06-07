c4b3e9c873fd4818edbb3484f27b6198cebaeb68
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-06-07 09:58:33 +0200

    queryString JS method

--- famille/static/js/utils.js
+++ famille/static/js/utils.js
@@ -2,6 +2,35 @@ function djangoUriParts() {
     return window.location.pathname.split("/");
 }
 
+// http://stackoverflow.com/questions/979975/how-to-get-the-value-from-url-parameter
+function queryString () {
+    // This function is anonymous, is executed immediately and
+    // the return value is assigned to QueryString!
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    if (!query) {
+        return query_string;
+    }
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        // If first entry with this name
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = pair[1];
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [query_string[pair[0]], pair[1]];
+            query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+            query_string[pair[0]].push(pair[1]);
+        }
+    }
+    return query_string;
+}
+
+
 module.exports = {
-    djangoUriParts: djangoUriParts
+    djangoUriParts: djangoUriParts,
+    queryString: queryString()
 }
