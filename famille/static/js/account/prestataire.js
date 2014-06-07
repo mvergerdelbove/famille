be3c42a1b3fbc98869f5c47a449b7a9a0c67c15b
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-06-07 09:59:00 +0200

    include favorite management in js for presta (oups)

--- famille/static/js/account/prestataire.js
+++ famille/static/js/account/prestataire.js
@@ -1,5 +1,6 @@
 var Shared = require("./shared.js");
 var Router = require("./router.js");
+var FavoriteView = require("./views/favorites.js");
 var PlanningView = require("./views/planning.js");
 var ProfilePic = require("./views/picture.js");
 var ReferenceView = require("./views/references.js");
@@ -52,6 +53,11 @@ var templateOutsideReference = '\
 
     function App () {
         this.router = new Router();
+        this.favorites = new FavoriteView({
+            el: $(".favorite-panel"),
+            modalEl: $("#modal-contact-favorite"),
+            router: this.router
+        });
         this.plannings = new PlanningView({
             el: $("#planning"),
             router: this.router
