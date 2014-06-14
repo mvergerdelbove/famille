994a1db31a6ad009d003f5c86ad4a6d66581a7c7
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-06-14 16:11:44 +0200

    deselect radio type on register page

--- famille/static/js/register.js
+++ famille/static/js/register.js
@@ -1,6 +1,7 @@
 (function ($){
     $(".social-link").click(function (e) {
         var type = $("[name=social_type]:checked").val();
+        if (!type) return;
         var href = this.href;
         this.href = href.replace(/next=\/([^\/]+)\/([^\/]+)\/([^\/]+)\//, function (all, action, backend, old_type) {
            return "next=/{action}/{backend}/{type}/"
@@ -19,9 +20,26 @@
 
     $(".btn-register").click(function (e) {
         var checkbox = $(this).data("checkbox");
-        if (!$(checkbox).is(':checked')) {
+        var name = ($(this).hasClass("btn-register-social")) ? "social_type" : "type";
+        function abort () {
             e.preventDefault();
             e.stopPropagation();
         }
+
+        if (!assertTypeChecked(name)) {
+            abort();
+            var $container = $("[name="+ name +"]").parents(".form-group");
+            if (!$(".text-danger", $container).length) {
+                $container.append("<span class='text-danger'>Veuillez sélectionner une valeur.</span>");
+            }
+        }
+        else if (!$(checkbox).is(':checked')) abort();
     });
+
+    function assertTypeChecked(name) {
+        // TODO: verify that this works on home page
+        if (!$("[type=checkbox][name="+ name +"]").length) return true;
+        return $("[name="+ name +"]:checked").length !== 0;
+    }
+
 })(jQuery);
